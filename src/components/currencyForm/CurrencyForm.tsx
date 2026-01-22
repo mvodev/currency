208398f3f8058c8738a0a7e81bdd5583205cc286
Vadim Manushin <vadimmanushin@yandex.ru>
2026-01-22 23:21:29 +0300

    added input for rub quantity

--- src/components/currencyForm/CurrencyForm.tsx
+++ src/components/currencyForm/CurrencyForm.tsx
@@ -1,19 +1,45 @@
 import { useForm, Controller } from 'react-hook-form';
 import Select from 'react-select';
+import type { StylesConfig  } from 'react-select';
 import type { JSONCurrencyData } from '../../types/types';
 import styles from './CurrencyForm.module.css';
 
 interface FormData {
   currencies: { label: string; value: string };
+  rubQuantity:number;
 }
 
 interface Props {
   currencies:JSONCurrencyData|undefined
 }
 
+const customStyles:StylesConfig = {
+  // Стилизация контейнера ввода
+  control: (base) => ({
+    ...base,
+    borderColor: 'blue',
+    '&:hover': { borderColor: 'darkblue' }, // hover эффект
+  }),
+  // Стилизация выпадающего меню
+  menu: (base) => ({
+    ...base,
+    backgroundColor: 'white',
+  }),
+  // Стилизация опций
+  option: (base, { isFocused, isSelected }) => ({
+    ...base,
+    backgroundColor: isSelected ? 'blue' : isFocused ? 'lightgray' : 'white',
+    color: isSelected ? 'white' : 'black',
+  }),
+};
+
 const CurrencyForm = ({currencies}:Props) => {
-  
-  const { control, handleSubmit } = useForm<FormData>();
+  const { 
+    control, 
+    handleSubmit, 
+    register, 
+    formState: { errors } 
+  } = useForm<FormData>();
   const onSubmit = (data:FormData) => {
     console.log(data.currencies.value); 
   };
@@ -27,20 +53,28 @@ const CurrencyForm = ({currencies}:Props) => {
 
   return (
     <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+      <label className={styles.formLabel}>
+      Введите количество в рублях:
+        <input className={styles.input}
+        {...register('rubQuantity', { required: true })}/>
+        {errors.rubQuantity?.type === 'required' && (
+          <span className={styles.error}>Введите значение</span>)}
+      </label>
       <label className={styles.formLabel}>Выберите валюту курс которой к рублю вы хотите посмотреть:</label>
-      <Controller
-        name="currencies"
-        control={control}
-        render={({ field }) => (
-          <Select
-            options={options}
-            value={field.value} 
-            onChange={field.onChange} 
-            onBlur={field.onBlur} 
-            isClearable
-          />
-        )}
-      />
+        <Controller
+          name="currencies"
+          control={control}
+          render={({ field }) => (
+            <Select
+              styles={customStyles}
+              options={options}
+              value={field.value} 
+              onChange={field.onChange} 
+              onBlur={field.onBlur} 
+              isClearable
+            />
+          )}
+        />
       {/* <button type="submit">Отправить</button> */}
     </form>
   );
